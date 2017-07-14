596d74265d12d2394060858487c9974be8dd6772
Allen B <pingguo.ny+git@gmail.com>
2017-07-13 21:41:27 -0400

    Create bot-web.js

---
+++ bot-web.js
@@ -0,0 +1,7 @@
+var BotWeb = {};
+BotWeb.sendMessage = function(chat_id, yes) {
+  var xhr = new XMLHttpRequest();
+  var promise = new Promise(function(resolve, reject) {
+    xhr.onload = function() {
+  });
+}
