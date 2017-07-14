544494bfd2867b642b749a0b72b71dc6f65aa253
Allen B <pingguo.ny+git@gmail.com>
2017-07-13 21:44:04 -0400

    Update bot-web.js

--- bot-web.js
+++ bot-web.js
@@ -1,7 +1,13 @@
 var BotWeb = {};
-BotWeb.sendMessage = function(chat_id, yes) {
+BotWeb.Bot = function(id) {this.id=id;}
+BotWeb.Bot.sendMessage = function(chat_id, text) {
   var xhr = new XMLHttpRequest();
   var promise = new Promise(function(resolve, reject) {
+    if(!"withCredentials" in xhr)
+      reject();
     xhr.onload = function() {
+      resolve(JSON.parse(xhr.responseText));
+    }
+    xhr.open("https://api.telegram.org/bot" + this.id + "/sendMessage?chat_id=" + chat_id + "&text=" + text);
   });
 }
